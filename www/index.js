fc02995d8298181eb0a548ef229763013f013256
Zachery Johnson <zacheryjohnson13@gmail.com>
2022-02-01 18:11:49 -0800

    Accept full URLs

--- www/index.js
+++ www/index.js
@@ -7,7 +7,7 @@ const button = document.getElementById('submit_button');
 const textOutput = document.getElementById('stats');
 
 button.onclick = () => {
-  const text = textInput.value;
+  const text = /[^/]*$/.exec(textInput.value)[0];
   const parsed = wasm.poe_parse(text);
   textOutput.innerText = parsed;
 };
